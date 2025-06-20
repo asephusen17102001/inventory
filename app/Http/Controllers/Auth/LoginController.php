7affb44321bffcfbc00da712008ecea4b5c411c6
asephusen17102001 <48233348+asephusen17102001@users.noreply.github.com>
2025-06-20 21:06:03 +0700

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -14,6 +14,7 @@ class LoginController extends Controller
         if (Auth::check()) {
             return redirect()->intended(Auth::user()->role . '/dashboard'); // atau sesuai role
         }
+        $data = [];
         return view('auth.login');
     }
 
